8612e3ac468519f91a7c9d0163bac2a1787e4f3d
timconspicuous <bipolarfuchs@gmail.com>
2025-08-10 00:48:15 +0200

    kv.atomic to mitigate accidental double posts

--- main.ts
+++ main.ts
@@ -10,23 +10,33 @@ if (!endpoint) {
 
 // @ts-ignore Deno.cron is unstable, run with --unstable-cron flag
 Deno.cron("QOTD", Deno.env.get("CRON_STRING"), async () => {
-	// Open KV and get last index
+	// Fetch  data
+	const response = await fetch(endpoint);
+	const { data } = await response.json();
+
+	// Open KV and fetch last index
 	// @ts-ignore Deno.openKv is unstable, run with --unstable-kv flag
 	const kv = await Deno.openKv(
 		// `https://api.deno.com/databases/${
 		// 	Deno.env.get("DENO_KV_DATABASE_ID")
 		// }/connect`,
 	);
-	const result = await kv.get<number>(["lastIndex"]);
-	let index = result.value ?? 0;
+	const current = await kv.get<number>(["lastIndex"]);
+	const index = current.value ?? 0;
+	const nextIndex = (index + 1) % data.length;
 
-	const response = await fetch(endpoint);
-	const { data } = await response.json();
+	// Perform atomic operation to update index
+	const result = await kv.atomic()
+		.check(current) // Ensure the value hasn't changed
+		.set(["lastIndex"], nextIndex)
+		.commit();
 
+	if (!result.ok) {
+		console.log("Another instance already updated the index, skipping");
+		return;
+	}
+
+	// Post to Discord webhook
 	await sendDiscordNotification(data[index].question);
 	console.log(`Cron: posted question of index ${index} to Discord.`);
-
-	// Update last index in KV
-	index = (index + 1) % data.length;
-	await kv.set(["lastIndex"], index);
 });
